e8b02c60b8f990a71c8394ac86afd674538ca81a
gmunster33 <gmunster33@gmail.com>
2020-02-03 22:17:40 -0800

    Crashing, but gets L3 Checkpoint 1; almost 2...

--- app/src/main/java/cs301/birthdaycake/CakeController.java
+++ app/src/main/java/cs301/birthdaycake/CakeController.java
@@ -1,13 +1,57 @@
 package cs301.birthdaycake;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+
 import cs301.birthdaycake.CakeView;
 
-public class CakeController {
+public class CakeController
+        implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+
     private CakeView cakeView;
     private CakeModel cakeModel;
+    private Button BLOW_OUTButton;
+    private Button GOODBYEButton;
+    private Switch FROSTINGSwitch;
+    private Switch CANDLESwitch;
+    private SeekBar CANDLESeekbar;
 
-    private void makeCakeView(CakeView view) {
-        cakeView = view;
-        cakeModel = view.getCakeModel();
-    }
-}
+     CakeController(CakeView view) {
+         cakeView = view;
+         cakeModel = view.getCakeModel();
+         GOODBYEButton.setOnClickListener(this);
+         BLOW_OUTButton.setOnClickListener(this);
+         CANDLESeekbar.setOnSeekBarChangeListener(this);
+     }
+
+         public void onClick (View v){
+             if (v == GOODBYEButton) {
+                 GOODBYEButton.setBackgroundColor(Color.BLUE);
+             }
+             if (v == BLOW_OUTButton) {
+                 Log.i("BLOW_OUTButton", "BLOW_OUTButton is working!");
+
+             }
+         }
+
+         public void onProgressChanged (SeekBar s,int val, boolean b){
+             GOODBYEButton.setBackgroundColor(Color.rgb(val * 10, val * 10, val * 10));
+         }
+         public void onStartTrackingTouch (SeekBar s){
+
+         }
+         public void onStopTrackingTouch (SeekBar s){
+
+         }
+     }
+
+
--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -13,56 +13,27 @@ import android.widget.Switch;
 
 import cs301.birthdaycake.CakeController;
 
-public class MainActivity extends AppCompatActivity // {
-implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity {
 
+    public CakeView Cake;
+    private CakeController controller;
     private Button BLOW_OUTButton;
     private Button GOODBYEButton;
     private Switch FROSTINGSwitch;
     private Switch CANDLESwitch;
     private SeekBar CANDLESeekbar;
-    private CakeController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
-
-        GOODBYEButton = (Button)findViewById(R.id.GOODBYE);
-        GOODBYEButton.setOnClickListener(this);
-
-        BLOW_OUTButton = (Button)findViewById(R.id.BLOW_OUT);
-        BLOW_OUTButton.setOnClickListener(this);
-
-        CANDLESwitch = (Switch)findViewById(R.id.candleswitch);
-
-        FROSTINGSwitch = (Switch)findViewById(R.id.frostingswitch);
-
-        CANDLESeekbar = (SeekBar)findViewById(R.id.candleseekBar);
-        CANDLESeekbar.setOnSeekBarChangeListener(this);
-
-        controller = new CakeController();
+        Cake = findViewById(R.id.cakeview);
+        controller = new CakeController(Cake);
+        GOODBYEButton = (Button) findViewById(R.id.GOODBYE);
+        BLOW_OUTButton = (Button) findViewById(R.id.BLOW_OUT);
+        CANDLESwitch = (Switch) findViewById(R.id.candleswitch);
+        FROSTINGSwitch = (Switch) findViewById(R.id.frostingswitch);
+        CANDLESeekbar = (SeekBar) findViewById(R.id.candleseekBar);
     }
-
-    public void onClick(View v) {
-        if (v == GOODBYEButton) {
-            GOODBYEButton.setBackgroundColor(Color.BLUE);
-            }
-        }
-
-
-    public void goodbye(View button) {
-    //    System.out.println("Goodbye");
-        Log.i("button", "Goodbye");
-    }
-    public void onProgressChanged(SeekBar s, int val, boolean b) {
-        GOODBYEButton.setBackgroundColor(Color.rgb(val*10,val*10,val*10));
-    }
-    public void onStartTrackingTouch(SeekBar s){
-
-        }
-    public void onStopTrackingTouch (SeekBar s) {
-
-        }
 }
